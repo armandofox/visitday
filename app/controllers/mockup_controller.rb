0a91966713009e2fdcd4c295ee3b8a5f14a55674
Armando Fox <fox@cs.berkeley.edu>
2010-12-18 18:45:14 -0800

    mockup for Staff page

--- app/controllers/mockup_controller.rb
+++ app/controllers/mockup_controller.rb
@@ -7,9 +7,7 @@ class MockupController < ApplicationController
 
   private
   def read_mockup_data
-    mockup_data = YAML::load(IO.read("#{RAILS_ROOT}/test/fixtures/mockup.yml"))
-    @areas = mockup_data["areas"]
-    @students = mockup_data["students"]
+    @params = YAML::load(IO.read("#{RAILS_ROOT}/test/fixtures/mockup.yml"))
   end
 
 end
